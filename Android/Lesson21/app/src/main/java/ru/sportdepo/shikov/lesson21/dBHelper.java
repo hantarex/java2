54cbf2a1cf9ba5e7e34b1ea3261143821fcff8a9
kond4r_home <kond4r@mail.ru>
2016-12-17 15:17:33 +0300

    Android21 v1.1

--- Android/Lesson21/app/src/main/java/ru/sportdepo/shikov/lesson21/dBHelper.java
+++ Android/Lesson21/app/src/main/java/ru/sportdepo/shikov/lesson21/dBHelper.java
@@ -3,18 +3,23 @@ package ru.sportdepo.shikov.lesson21;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 public class dBHelper extends SQLiteOpenHelper {
 
     private static final String LESSON21_DB = "lesson21.db";
+    private static final int VERSION = 1;
 
-    public dBHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, LESSON21_DB, factory, version);
+    public dBHelper(Context context) {
+        super(context, LESSON21_DB, null, VERSION);
+        Log.e("DB","Create DB");
     }
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-
+        Log.e("DB","Start create table");
+        ContentTable.createTable(sqLiteDatabase);
+        Log.d("DB","Create table!");
     }
 
     @Override
